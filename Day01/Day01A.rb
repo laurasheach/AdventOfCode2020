53864dfb91cac50ee773ded46d12a12eaccc9585
laurasheach <laura_bubble_git@yahoo.com>
2020-12-02 21:04:26 +0000

    Day 1 - Part 1 Complete

--- Day01/Day01A.rb
+++ Day01/Day01A.rb
@@ -1,1 +1,54 @@
 # Advent of Code Day 1 - Puzzle 1
+
+# Before you leave, the Elves in accounting just need you to fix your expense report
+# (your puzzle input); apparently, something isn't quite adding up.
+#
+# Specifically, they need you to find the two entries that sum to 2020 and then multiply
+# those two numbers together.
+#
+# For example, suppose your expense report contained the following:
+#
+# 1721
+# 979
+# 366
+# 299
+# 675
+# 1456
+#
+# In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together
+# produces 1721 * 299 = 514579, so the correct answer is 514579.
+#
+# Of course, your expense report is much larger. Find the two entries that sum to 2020;
+# what do you get if you multiply them together?
+
+## Note - I have gone beyond the requirements in order to practice my code & assist my learning
+
+# require ('pry')
+
+  # expenses = [1721, 979, 366, 299, 675, 1456]
+  # total = 2020
+
+def get_highest_number(expenses)
+  return expenses.max_by { |expense| expense }
+end
+
+def get_total(expenses)
+
+  sum = 0
+
+  for expense in expenses
+    sum += expense
+  end
+
+  return sum
+
+end
+
+def get_twenty_twenty(expenses, total)
+
+  arr = expenses.combination(2).detect { |expense1, expense2| expense1 + expense2 == total }
+  multiplied_total = arr[0] * arr[1]
+
+  return "These two numbers which add up to #{total} when multiplied gives us #{multiplied_total}"
+
+end
--- Day01/Day01A_spec.rb
+++ Day01/Day01A_spec.rb
@@ -4,6 +4,24 @@ require_relative('Day01A')
 
 class Day01A < MiniTest::Test
 
+  def setup
 
+    @expenses1 = [1721, 979, 366, 299, 675, 1456]
+    @expenses2 = [625, 966, 1122, 489, 736, 1568, 351, 1531, 210, 668, 966]
+
+  end
+
+  def test_get_max_number
+    assert_equal(1721, get_highest_number(@expenses1))
+  end
+
+  def test_get_total
+    assert_equal(5496, get_total(@expenses1))
+  end
+
+  def test_get_twenty_twenty
+    assert_equal("These two numbers which add up to 2020 when multiplied gives us 514579", get_twenty_twenty(@expenses1,2020))
+    assert_equal("These two numbers which add up to 2020 when multiplied gives us 748659", get_twenty_twenty(@expenses2,2020))
+  end
 
 end
